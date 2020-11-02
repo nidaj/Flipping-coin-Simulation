17bdd1db85dd8addc05622c936760b1d605ed1a1
nida.j <nida.jawre@gmail.com>
2020-11-02 23:14:15 +0530

    Modified: Flipped the coin n user_input number of times

--- FlippingCoinSimulator.sh
+++ FlippingCoinSimulator.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 echo "This problem displays the winner Heads or Tails"
 flip=$((RANDOM%2))
-if (( $flip == 1 ))
-then
-	echo "Heads Wins"
-else
-	echo "Tail Wins"
-fi
+h=0
+t=0
+read -p "Enter number of times u wnat to flip the coin: " n
+for (( f=1; f<=n; f++ ))
+do
+	flip=$((RANDOM%2))
+	if (( $flip == 1 ))
+	then
+		((h++))
+	else
+		((t++))
+	fi
+done
+echo "Head wins: "$h "times"
+echo "Tail wins: "$t "times"
